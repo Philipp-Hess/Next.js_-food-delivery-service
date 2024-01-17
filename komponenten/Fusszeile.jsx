519be8083913641e432353a0bdb67d62b70a6abf
Philipp Hess <philipp-hess@hotmail.de>
2024-01-17 16:06:42 +0100

    added sass

---
+++ komponenten/Fusszeile.jsx
@@ -0,0 +1,7 @@
+export default function Fusszeile() {
+  return (
+    <div className="d-flex justtify-content-center fixed-bottom text-secondary bg-light">
+      <h6>🥡 Liefermax | 📞 030/3696771 | 🕙 Mo-So: 10:00-22:00 </h6>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "@/components/Layout";
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from '../komponenten/Layout'
+import '../styles/custom.scss'
 
-export default function App({ Component, pageProps }) {
-  return;
-  <Layout>
-    <Component {...pageProps} />;
-  </Layout>;
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+
+export default MyApp
