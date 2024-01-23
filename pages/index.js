fed80726ac85434c6bc99efd7a7274becf0cd96c
Philipp Hess <philipp-hess@hotmail.de>
2024-01-23 19:58:19 +0100

    read-product-data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Slider from "@/komponenten/Slider";
 import Produktliste from "@/komponenten/Produktliste";
+import mongodb from "@/utils/mongodb";
+import Produkt from "@/models/Produkt";
 
-export default function Home() {
+export default function Home({ produkte }) {
   return (
     <div>
       <Slider />
-      <Produktliste />
+      <Produktliste produkte={produkte} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  await mongodb.dbConnect();
+  const produkte = await Produkt.find({}).lean();
+  return {
+    props: {
+      produkte: JSON.parse(JSON.stringify(produkte)),
+    },
+  };
 }
